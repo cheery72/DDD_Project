376b0264276feb582c95ccb0b096f7979473d32e
김경민 <nutmy52@naver.com>
2022-10-25 23:31:08 +0900

    feature: Find Member Board
    멤버 게시글 조회

--- ddd/src/main/java/com/project/ddd/board/root/BoardRepository.java
+++ ddd/src/main/java/com/project/ddd/board/root/BoardRepository.java
@@ -2,7 +2,12 @@ package com.project.ddd.board.root;
 
 import com.project.ddd.board.root.Board;
 import com.project.ddd.board.value.BoardId;
+import com.project.ddd.board.value.Boarder;
+import com.project.ddd.member.value.MemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, BoardId> {
+    Optional<Board> findByBoarder(Boarder boarder);
 }
