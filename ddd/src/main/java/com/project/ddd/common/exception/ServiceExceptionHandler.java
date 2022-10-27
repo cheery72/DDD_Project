9477dd2c76ce341fd3b7f9024731377c58a971ed
김경민 <nutmy52@naver.com>
2022-10-27 16:03:21 +0900

    feature: Find Boarddetail
     게시글 디테일 정보 조회 API

---
+++ ddd/src/main/java/com/project/ddd/common/exception/ServiceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.project.ddd.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class ServiceExceptionHandler {
+
+    @ExceptionHandler(NoSuchBoardException.class)
+    protected ResponseEntity<?> handleNoSuchBoardException(NoSuchBoardException e){
+        final ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("Board Not Found")
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+}
