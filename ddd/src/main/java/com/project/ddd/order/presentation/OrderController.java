87750357894b52edfd8881eb1baa9a07bff1fe82
김경민 <nutmy52@naver.com>
2022-10-29 21:51:32 +0900

    feature: controller order create test
    주문 컨트롤러 주문 생성 테스트

--- ddd/src/main/java/com/project/ddd/order/presentation/OrderController.java
+++ ddd/src/main/java/com/project/ddd/order/presentation/OrderController.java
@@ -1,4 +1,31 @@
 package com.project.ddd.order.presentation;
 
+
+import com.project.ddd.order.application.dto.OrderCreateDto;
+import com.project.ddd.order.application.dto.OrderDetailDto;
+import com.project.ddd.order.root.OrderService;
+import com.project.ddd.order.value.OrderStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/order")
+@RestController
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> orderCreate(@RequestBody OrderCreateDto orderCreateDto){
+        log.info("order create start ----");
+
+        orderService.createOrder(orderCreateDto);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
